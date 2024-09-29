ecbb7575f2c1f683ae44fc5e7b25964cf1f99cf8
Mauricio <mauricio.furlan@hotmail.com>
2024-09-28 21:22:59 -0300

    test

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
-const axios = require('axios'); 
-const { HttpsProxyAgent } = require('https-proxy-agent'); 
+const cors = require('cors');
+const axios = require('axios');
+const { HttpsProxyAgent } = require('https-proxy-agent');
 
 const app = express();
 app.use(cors({
     origin: '*'
-}))
+}));
 
-// analisa o corpo da requisição (dados do formulário)
-app.use(express.urlencoded({ extended: true} ));
+// Analisa o corpo da requisição (dados do formulário)
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
-const proxyUrl = 'https://47.88.31.196:8080'; // Exemplo: http://proxy.example.com:8080
-const agent = new HttpsProxyAgent(proxyUrl);
+// Definindo o proxy com autenticação
+const proxyHost = '208.195.174.227'; // IP do seu proxy ToolIP
+const proxyPort = 65095;             // Porta do seu proxy ToolIP
+const proxyUsername = process.env.PROXY_USERNAME;  // Usuário fornecido pela ToolIP
+const proxyPassword = process.env.PROXY_PASSWORD;    // Senha fornecida pela ToolIP
+
+const proxyUrl = `http://${proxyUsername}:${proxyPassword}@${proxyHost}:${proxyPort}`;
+const agent = new HttpsProxyAgent(proxyUrl); // Cria um agente de proxy com autenticação
 
 app.get('/', (req, res) => {
-    res.send('ok')
-})
-
+    res.send('ok');
+});
 
 app.post('/scrapping', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     const formData = {
         currentpage: req.body.currentpage || 1,
         order_column: req.body.order_column || 101,
@@ -35,18 +41,17 @@ app.post('/scrapping', (req, res) => {
             'Content-Type': 'application/x-www-form-urlencoded',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         },
-        httpsAgent: agent
+        httpsAgent: agent  // Configurando o agente proxy
     }).then(response => response.data)
     .then(html => {
-        res.send(html)
+        res.send(html);
     })
     .catch(error => {
-        console.error('Erro ao fazer scraping:', error);
+        console.error('Erro ao fazer scraping:', error.message);
         res.status(500).send('Erro ao fazer scraping');
     });
-
-})
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
-})
+});
